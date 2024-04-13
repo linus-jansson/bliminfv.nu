2b83c1d7727df89b390a32de56aab35c0ffc2b13
Linus Jansson <contact@limpan.dev>
2024-04-13 20:38:47 +0200

    some improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef } from 'react'
 import { createPortal } from 'react-dom';
 
-const Btn = React.forwardRef<HTMLButtonElement, React.ButtonHTMLAttributes<HTMLButtonElement>>(({children, onClick, ...props}, ref) => {
+const Btn = React.forwardRef<HTMLButtonElement, React.ButtonHTMLAttributes<HTMLButtonElement>>(({ children, onClick, ...props }, ref) => {
     return (
-        <button 
+        <button
             ref={ref}
             className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
             onClick={onClick} {...props}
@@ -13,15 +13,15 @@ const Btn = React.forwardRef<HTMLButtonElement, React.ButtonHTMLAttributes<HTMLB
 
 const ShyBtn = () => {
     const btnRef = useRef<HTMLButtonElement>(null);
-    
-    const getCoordinates = (e :any) => {
+
+    const getCoordinates = (e: any) => {
         if (e.touches) {
             return { x: e.touches[0].clientX, y: e.touches[0].clientY };
         }
         return { x: e.clientX, y: e.clientY };
     }
 
-    const handleMove = (e :any) => {
+    const handleMove = (e: any) => {
         const { x: mouseX, y: mouseY } = getCoordinates(e);
         const button = btnRef.current;
         if (!button) return;
@@ -56,12 +56,12 @@ const ShyBtn = () => {
     }, []);
 
     const spawnSwiningText = () => {
-        const alreadyExists = document.getElementById('nu-uh');
-        if (alreadyExists) return;
+        // const alreadyExists = document.getElementById('nu-uh');
+        // if (alreadyExists) return;
 
         // If ShyBtn is clicked, create a new element with the text "Nu-uh" and append it to the body
         const nuUh = document.createElement('div');
-        nuUh.id = 'nu-uh';
+        // nuUh.id = 'nu-uh';
         nuUh.style.position = 'absolute';
         nuUh.style.top = `${btnRef.current!.getBoundingClientRect().top + 15}px`;
         nuUh.style.left = `${btnRef.current!.getBoundingClientRect().left + 15}px`;
@@ -83,9 +83,9 @@ const ShyBtn = () => {
     }
 
     return (
-        <Btn 
-            ref={btnRef} 
-            style={{ 
+        <Btn
+            ref={btnRef}
+            style={{
                 position: 'static',
                 transition: 'left 100ms, top 100ms',
                 willChange: 'left, top'
@@ -108,13 +108,13 @@ const MovingCloud = () => {
         const moveCloud = () => {
             const cloud = cloudRef.current;
             if (!cloud) return;
-            
+
             const cloudRect = cloud.getBoundingClientRect();
             const newX = cloudRect.left + 1; // Move cloud to the right by 1 pixel
-            
-            if (newX > window.innerWidth - cloudRect.width) {
+
+            if (newX > (window.visualViewport?.width || window.innerWidth)) {
                 // Reset the cloud to start from the left again with a random vertical position
-                cloud.style.left = `-50px`;
+                cloud.style.left = `-${cloudRect.width}px`;
                 cloud.style.top = `${Math.floor(Math.random() * (window.innerHeight - cloudRect.height))}px`;
             } else {
                 cloud.style.left = `${newX}px`; // Update the left style to move the cloud
@@ -122,7 +122,7 @@ const MovingCloud = () => {
         };
 
         const intervalId = setInterval(moveCloud, 30); // Use setInterval for continuous movement
-        
+
         return () => clearInterval(intervalId); // Clear interval on component unmount
     }, []);
 
@@ -131,8 +131,8 @@ const MovingCloud = () => {
     const randomStartY = Math.floor(Math.random() * window.innerHeight);
 
     return (
-        <div 
-            ref={cloudRef}  
+        <div
+            ref={cloudRef}
             style={{
                 position: 'absolute',
                 top: `${randomStartY}px`,
@@ -140,19 +140,18 @@ const MovingCloud = () => {
                 willChange: 'left',
                 transition: 'left 30ms',
             }}
-        >   
+        >
             <i className="cloud"></i>
         </div>
     );
 };
 
 const PortalContainer = () => {
+    const randomAmountOfCloudsBetween5and25 = Math.floor(Math.random() * 20) + 5;
     return (
-    <>
-        {Array.from({ length: 25 }).map((_, i) => (
+        Array.from({ length: randomAmountOfCloudsBetween5and25 }).map((_, i) => (
             <MovingCloud key={i} />
-        ))}
-    </>
+        ))
     )
 }
 
@@ -179,15 +178,31 @@ const Game = () => {
 //     )
 // }
 
+const Link = ({ href, children }: { href: string, children: React.ReactNode }) => {
+    return (
+        <a href={href} target="_blank" rel="noreferrer" className='text-blue-500 hover:underline'>{children}</a>
+    )
+}
+
+const Spacer = () => {
+    return <div style={{ height: '1rem' }} />
+}
+
 function App() {
     return (
         <>
             <main className="grid place-items-center w-screen h-screen bg-blue-300 ">
                 <div className='z-50 space-y-10'>
-                    <Game/>
+                    <Game />
                 </div>
             </main>
-            {createPortal(<PortalContainer/>, document.body)}
+            <footer className='absolute bottom-0 left-0 w-full z-50'>
+                <Spacer />
+                <div className='text-center text-gray-700 p-2 text-sm'>
+                    <p>Made with ❤️ by <Link href="https://www.limpan.dev/">l1mpan 🍞</Link></p>
+                </div>
+            </footer>
+            {createPortal(<PortalContainer />, document.body)}
         </>
     )
 }
